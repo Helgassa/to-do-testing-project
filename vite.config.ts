a21067f253e7b2b544848fffb49dca240cbb2251
Elena Gromova <elena.gromova@hexlet.io>
2024-09-25 23:50:07 +0300

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/to-do-testing-project/',
+  base: './',
   plugins: [react()],
 })
